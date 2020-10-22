0a21e5484ed74c9ac84406c437f45b063cb17add
atusy <30277794+atusy@users.noreply.github.com>
2020-10-22 22:04:43 +0900

    fix shading.color not applied on inline code (patches #25)

--- inst/lua/inline-code.lua
+++ inst/lua/inline-code.lua
@@ -2,7 +2,7 @@ function Code(elem)
   if elem.attributes['font.family'] == nil then
     elem.attributes['font.family'] = 'monospace'
   end
-  if elem.attributes['font.family'] == nil then
+  if elem.attributes['shading.color'] == nil then
     elem.attributes['shading.color'] = '#f8f8f8'
   end
   local span = pandoc.Span(elem.text)
