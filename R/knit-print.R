d16c1b3f57dd8efbbbaaa451fae68ff8fbd5f2fe
atusy <30277794+atusy@users.noreply.github.com>
2021-03-11 00:05:39 +0900

    add helpful message to suggest results="asis"

--- R/knit-print.R
+++ R/knit-print.R
@@ -1,10 +1,19 @@
 knit_print.ftExtra <- function(x, options, ...) {
-  if (options$results == "asis") {
-    cat(sprintf(
-      '\n\n---\nftExtra-ref-%s: "%s"\n---\n\n',
-      options$label,
-      attr(x, "citations", exact = TRUE)
-    ))
+  citations <- attr(x, "citations", exact = TRUE)
+  if (citations != "") {
+    if (options$results == "asis") {
+      cat(sprintf(
+        '\n\n---\nftExtra-ref-%s: "%s"\n---\n\n',
+        options$label,
+        citations
+      ))
+    } else {
+      message(
+        "Set the chunk option, `results='asis'`, ",
+        "in order to automatically add citations to reference ",
+        "with `ftExtra::colformat_md`."
+      )
+    }
   }
   NextMethod("knit_print", x, options, ...)
 }
