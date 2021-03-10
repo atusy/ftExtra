2f51c2f84c8f036d31f2174ea7354f15bb383577
atusy <30277794+atusy@users.noreply.github.com>
2021-03-11 00:28:21 +0900

    remove the limitation and add hidden document

--- R/knit-print.R
+++ R/knit-print.R
@@ -1,19 +1,27 @@
-knit_print.ftExtra <- function(x, options, ...) {
-  citations <- attr(x, "citations", exact = TRUE)
-  if (citations != "") {
-    if (options$results == "asis") {
-      cat(sprintf(
-        '\n\n---\nftExtra-ref-%s: "%s"\n---\n\n',
-        options$label,
-        citations
-      ))
-    } else {
-      message(
-        "Set the chunk option, `results='asis'`, ",
-        "in order to automatically add citations to reference ",
-        "with `ftExtra::colformat_md`."
-      )
-    }
-  }
-  NextMethod("knit_print", x, options, ...)
+#' knit_print for extended flextable object
+#'
+#' Adds a YAML meta data block to cite materials found by `colformat_md()` in
+#' the `flextable` object.
+#'
+#' @inheritParams kntir::knit_print
+#' @param label
+#'  A key to be set in the YAML meta data block to cite materials.
+#'  This must be unique in the document.
+#'  Default value is the chunk label prepended by "ftExtra-cite-".
+#'
+#' @return The `knit_asis` class object.
+#' @noRd
+knit_print.ftExtra <- function(x,
+                               options,
+                               ...,
+                               key = paste0("ftExtra-cite-", options$label)) {
+  ft <- NextMethod("knit_print", x, options, ...)
+
+  cite <- attr(x, "citations", exact = TRUE)
+
+  if (cite == "") return(ft)
+
+  res <- sprintf('---\n%s: "%s"\n---\n\n%s', key, cite, ft)
+  attributes(res) <- attributes(ft)
+  res
 }
