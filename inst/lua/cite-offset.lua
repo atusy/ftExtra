ba14e4c83cd217d9ed8d46cc169a4308b3e3f7c2
atusy <30277794+atusy@users.noreply.github.com>
2021-03-11 21:47:30 +0900

    cite-offset.lua substitutes string

--- inst/lua/cite-offset.lua
+++ inst/lua/cite-offset.lua
@@ -2,8 +2,13 @@ function Meta(meta)
   offset = meta.citationNoteNumOffset or 0
 end
 
+function add(x)
+  return math.floor(tonumber(x) + offset)
+end
+
 function Cite(cite)
   cite.citations[1].note_num = math.floor(offset + cite.citations[1].note_num)
+  cite.content[1].text = cite.content[1].text:gsub("%d+", add)
   return cite
 end
 
