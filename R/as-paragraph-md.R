57206209eafa23aa46b75b2a7490926e6184ce35
atusy <30277794+atusy@users.noreply.github.com>
2022-01-16 07:59:33 +0900

    add support for input formats without div

--- R/as-paragraph-md.R
+++ R/as-paragraph-md.R
@@ -126,6 +126,7 @@ as_paragraph_md <- function(x,
       organize(md_df, auto_color_link, .footnote_options)
     } else {
       lapply(x, function(x) {
+        if (x == "") return(construct_chunk(list()))
         y <- x %>%
           md2df(pandoc_args = pandoc_args, .from = .from, .check = TRUE) %>%
           solve_footnote(.footnote_options, auto_color_link) %>%
