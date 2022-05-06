5a40fa5ccb613bc17bbf8da73001ca19ca706eab
atusy <30277794+atusy@users.noreply.github.com>
2022-05-06 23:43:47 +0900

    add warning on metadata with chunks and inline chunks

--- R/md2ast.R
+++ R/md2ast.R
@@ -10,7 +10,7 @@ md2ast <- function(x,
 
   front_matter <- if ((length(metadata) > 0) && support_yaml(.from)) {
     yaml::write_yaml(metadata, tf)
-    c("---", xfun::read_utf8(tf), "---", "", "")
+    c("---", warn_chunk(xfun::read_utf8(tf)), "---", "", "")
   }
 
   xfun::write_utf8(c(front_matter, x), tf)
@@ -20,10 +20,26 @@ md2ast <- function(x,
     to = "json",
     from = .from,
     output = tf,
-    citeproc = !is.null(metadata$bibliography) || any(grepl("^--bibliography", pandoc_args)),
+    citeproc = !is.null(metadata[["bibliography"]]) || any(grepl("^--bibliography", pandoc_args)),
     options = pandoc_args,
     wd = getwd()
   )
 
   jsonlite::read_json(tf, simplifyVector = FALSE)
+}
+
+
+warn_chunk <- function(text) {
+  purl <- withr::with_options(list(knitr.purl.inline = TRUE),
+                              knitr::purl(text = text, quiet = TRUE))
+
+  if (purl != "") {
+    warning(
+      "The metadata argument of colformat_md or as_paragraph_md should not ",
+      "contain chunks or inline chunks because of potential errors on Pandoc.",
+      "Consider replacing them with evaluated values."
+    )
+  }
+
+  return(text)
 }
