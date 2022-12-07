097ccce979c4ec3d3a4b4c18396eb83fe0aa01df
atusy <30277794+atusy@users.noreply.github.com>
2022-12-07 09:23:16 +0900

    workaround error from Fedora Linux, R-devel, GCC on R-hub

--- tests/testthat/test-md2ast.R
+++ tests/testthat/test-md2ast.R
@@ -4,20 +4,25 @@ test_with_pandoc("citation", {
   current_dir <- getwd()
   suppressWarnings(knitr::write_bib("ftExtra", temp_file))
 
-  local({
-    on.exit(setwd(current_dir))
-    setwd(temp_dir)
-    expect_identical(
-      md2ast(
-        "@R-ftExtra",
-        pandoc_args = c(
-          "--bibliography", temp_file
-        )
-      )$blocks,
+  res_current_dir <- try(md2ast(
+    "@R-ftExtra",
+    pandoc_args = c("--bibliography", temp_file)
+  )$blocks)
+
+  # NOTE: Avoid strange errors from some environments
+  # (e.g., Fedora Linux, R-devel, GCC on R-hub)
+  # > pandoc-citeproc: Error in $: Incompatible API versions:
+  # > encoded with [1,20] but attempted to decode with [1,17,0,4].
+  if (class(res_current_dir) != "try-error") {
+    res_temp_dir <- local({
+      on.exit(setwd(current_dir))
+      setwd(temp_dir)
       md2ast(
         "@R-ftExtra",
         metadata = list(bibliography = basename(temp_file))
       )$blocks
-    )
-  })
+    })
+
+    expect_identical(res_current_dir, res_temp_dir)
+  }
 })
