dce4da01db218ca4577bab12cae7d0645b39edcb
atusy <30277794+atusy@users.noreply.github.com>
2023-03-14 00:12:53 +0900

    chore: prefer <- to =

--- R/md2df.R
+++ R/md2df.R
@@ -112,10 +112,10 @@ format_by_attr <- function(x) {
 
   if (length(a) == 0L) return(x)
 
-  x$Underline = any(x$Underline, a$class == "underline")
-  x$color = last(a$color) %||% NA_character_
-  x$shading.color = last(a$shading.color) %||% NA_character_
-  x$font.family = last(a$font.family) %||% NA_character_
+  x$Underline <- any(x$Underline, a$class == "underline")
+  x$color <- last(a$color) %||% NA_character_
+  x$shading.color <- last(a$shading.color) %||% NA_character_
+  x$font.family <- last(a$font.family) %||% NA_character_
 
   x
 }
